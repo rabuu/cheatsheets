b3893f034f6a33a11ecab456630fa71f3c7a063c
Rasmus Buurman <rasmus@rbuurman.de>
2025-10-16 15:19:17 +0200

    Use nix and direnv

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Cheatsheets für meine Uni-Klausuren";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            texlive.combined.scheme-full
+            texlab
+          ];
+        };
+      }
+    );
+}
